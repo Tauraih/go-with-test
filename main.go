cb69bf1552b4b14553cc98de68bcc5d08a7e633e
Taurai <tauraihv@gmail.com>
2021-05-29 01:16:47 +0200

    maps

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
-package main 
+package main
 
-import ( 
+import (
+	"errors"
 	"fmt"
 	"math"
-	"errors"
 )
 
 const enHello = "Hello, "
@@ -136,3 +136,16 @@ func (w *Wallet) Withdraw(amount Bitcoin) error {
     w.balance -= amount
     return nil
 }
+
+type Dictionary map[string]string
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string , error) {
+	definition, ok := d[word]
+	if !ok {
+		return " ", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) {}
---
+++ map_test.go
@@ -0,0 +1,52 @@
+package main
+
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "another test"}
+
+	t.Run("Known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "another test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+// func TestAdd(t *testing.T) {
+// 	dicttionary := Dictionary{}
+// 	dicttionary.Add("test", "just a test")
+
+// 	want := "just a test"
+// 	got, err := dicttionary.Search("test")
+// 	if err != nil {
+// 		t.Fatal("should find added word:", err)
+// 	}
+
+// 	if got != want {
+// 		t.Errorf("got %q want %q", got, want)
+// 	}
+// }
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
